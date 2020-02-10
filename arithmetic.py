f26e6cf39078316ff28482a779dccde9efd840a7
HayatoG <guilherme.ryders@gmail.com>
2020-02-10 19:39:31 -0300

    Aula de operadores e entrada de dados

---
+++ arithmetic.py
@@ -0,0 +1,33 @@
+#Aula 10/02/2020
+
+# + -Soma
+n1 = 10
+n2 = 2
+soma = n1 + n2
+
+print("Soma:",soma)
+
+# - -Subtração
+subt = n1 - n2
+
+print("Subtração:",subt)
+
+# * -Multiplicação
+mult = n1 * n2
+
+print("Multiplicação:",mult)
+
+# / -Divisão
+div = n1/n2
+
+print("Divisão:",div)
+
+# ** -Exponenciação
+exp = n1**n2
+
+print("Exponenciação:",exp)
+
+# % -Módulo
+mod = (n1%(n2+1))
+
+print("Resto da divisão:", mod)
---
+++ input.py
@@ -0,0 +1,13 @@
+#Aula 10/02/2020
+
+# Input()
+
+#String
+  #print(type(name)) - str
+name = input("Digite seu nome: ")
+print(name)
+
+#Inteiro
+  #year = int(input("Digite sua idade: "))int(input("Digite sua idade: "))
+year = int(input("Digite sua idade: "))
+print(year)
